462af8cd706e1181038bf2c03bb05fa0b4ce80b9
Aracturus <iconwebdev1@gmail.com>
2021-10-14 18:05:30 +0530

    Template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import CompOne from './Components/CompOne';
+import CompTwo from './Components/CompTwo';
+import CompThree from './Components/CompThree';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <CompOne/>
+    <CompTwo/>
+    <CompThree/>
+      
+    </>
   );
 }
 
---
+++ src/Components/CompOne.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+export default function CompOne() {
+    return (
+        <>
+         <section className="hero is-small is-dark">
+        <div className="hero-body"> 
+                <p className="title">
+            Medium hero
+          </p>
+          <p className="subtitle">
+            Medium subtitle
+          </p>
+        </div>
+          </section>
+
+        <section className="hero is-small is-dark">
+        <div className="hero-body">
+      
+
+          <div className="columns">
+              <div className="column">
+                  <img  src={'https://images.pexels.com/photos/1181271/pexels-photo-1181271.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'} alt="" srcset=""/> 
+
+              </div>
+              <div className="column">
+                  <p>
+                      <h1>Lorem Paragraph</h1>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius repellat enim cupiditate et inventore, distinctio ullam debitis, at expedita architecto magni eum deleniti possimus tempora earum reiciendis? Sunt, saepe nostrum!
+                      Maiores minima necessitatibus suscipit, numquam asperiores voluptates. Accusantium ratione, rerum doloribus nemo iste adipisci eum facilis ab corrupti laboriosam ut dolorem qui harum ipsa numquam odit et? Porro, saepe minima.
+                      Error, est odit. Accusamus, aut adipisci aperiam repellendus sunt consequatur esse vitae dolores. Id ratione, natus perspiciatis consequatur dignissimos explicabo dolores facilis unde vel ut tempora, incidunt atque ducimus maxime.
+                      Quibusdam repellendus eligendi commodi, voluptatem laboriosam et sed iste temporibus numquam unde eos porro quo delectus facere sapiente ratione at earum sequi alias assumenda magnam 
+                  </p>
+
+              </div>
+          </div>
+        </div>
+      </section>
+            
+        </>
+    )
+}
